7c161b9a85a0aeaf290bf496f885838afefc4188
DestinedGamerz <61101826+DestinedGamerz@users.noreply.github.com>
2020-02-17 16:42:31 -0500

    Add files via upload

---
+++ Mafia.py
@@ -0,0 +1,108 @@
+import random
+
+game_choices = ['Shoot', 'Save', 'Help']
+
+pick = random.choice(game_choices)
+
+pick_2 = input("Enter yur target's name: ")
+
+if pick == 'Shoot' and pick_2 == 'Alice':
+    print("You have shot Alice")
+
+if pick == 'Shoot' and pick_2 == 'Brandon':
+    print("You have shot Brandon")
+
+if pick == 'Shoot' and pick_2 == 'Edward':
+    print("You have shot Edward")
+
+if pick == 'Shoot' and pick_2 == 'Percy':
+    print("You have shot Percy")
+
+if pick == 'Shoot' and pick_2 == 'Annabeth':
+    print("You have shot Annabeth")
+
+if pick == 'Shoot' and pick_2 == 'Piper':
+    print("You have shot Piper")
+
+if pick == 'Shoot' and pick_2 == 'Leo':
+    print("You have shot Leo")
+
+if pick == 'Shoot' and pick_2 == 'Jake':
+    print("You have shot Jake")
+
+if pick == 'Shoot' and pick_2 == 'Frank':
+    print("You have shot Frank")
+
+if pick == 'Shoot' and pick_2 == 'Reyna':
+    print("You have shot Reyna")
+
+if pick == 'Shoot' and pick_2 == 'Rachel':
+    print("You have shot Rachel")
+
+if pick == 'Save' and pick_2 == 'Alice':
+    print("You have saved Alice")
+
+if pick == 'Save' and pick_2 == 'Brandon':
+    print("You have saved Brandon")
+
+if pick == 'Save' and pick_2 == 'Edward':
+    print("You have saved Edward")
+
+if pick == 'Save' and pick_2 == 'Percy':
+    print("You have saved Percy")
+
+if pick == 'Save' and pick_2 == 'Annabeth':
+    print("You have saved Annabeth")
+
+if pick == 'Save' and pick_2 == 'Piper':
+    print("You have saved Piper")
+
+if pick == 'Save' and pick_2 == 'Leo':
+    print("You have saved Leo")
+
+if pick == 'Save' and pick_2 == 'Jake':
+    print("You have saved Jake")
+
+if pick == 'Save' and pick_2 == 'Frank':
+    print("You have saved Frank")
+
+if pick == 'Save' and pick_2 == 'Reyna':
+    print("You have saved Reyna")
+
+if pick == 'Save' and pick_2 == 'Rachel':
+    print("You have saved Rachel")
+
+if pick == 'Help' and pick_2 == 'Alice':
+    print("You have helped Alice")
+
+if pick == 'Help' and pick_2 == 'Brandon':
+    print("You have helped Brandon")
+
+if pick == 'Help' and pick_2 == 'Edward':
+    print("You have helped Edward")
+
+if pick == 'Help' and pick_2 == 'Percy':
+    print("You have helped Percy")
+
+if pick == 'Help' and pick_2 == 'Annabeth':
+    print("You have helped Annabeth")
+
+if pick == 'Help' and pick_2 == 'Piper':
+    print("You have helped Piper")
+
+if pick == 'Help' and pick_2 == 'Leo':
+    print("You have helped Leo")
+
+if pick == 'Help' and pick_2 == 'Jake':
+    print("You have helped Jake")
+
+if pick == 'Help' and pick_2 == 'Frank':
+    print("You have helped Frank")
+
+if pick == 'Help' and pick_2 == 'Reyna':
+    print("You have heled Reyna")
+
+if pick == 'Help' and pick_2 == 'Rachel':
+    print("You have helped Rachel")
+
+print("You have chose to", pick)
